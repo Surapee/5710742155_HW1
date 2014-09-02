21bc387a6079c38951d0c413002c289721b9d2dd
onnttnno <sanamori@msn.com>
2014-09-02 19:34:40 +0700

    5710742155_EX1.9

---
+++ 5710742155_EX1.9/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a;
+    printf("Input float number: ");
+    scanf("%f",&a);
+    printf("%10.3f\n",a);
+    system("pause");
+    return 0;
+}
